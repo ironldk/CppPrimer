7d3bbd07a88907700c42b5db8c6171242e1d0908
ironldk <ldk953146716@126.com>
2022-04-22 00:00:11 +0800

    using Visual Studio's Git

--- Ch16/16_54.cpp
+++ Ch16/16_54.cpp
@@ -31,15 +31,68 @@ using std::endl;
 //	return 0;
 //}
 
-void set(char *c, char s) {
-	c = new char;
-	*c = s;
+//void set(char *c, char s) {
+//	c = new char;
+//	*c = s;
+//}
+//
+//int main() {
+//	char* c = '\0';
+//	char s = 'k';
+//	set(c, s);
+//	cout << c;
+//	return 0;
+//}
+#include <string>
+#include <vector>
+#include <io.h>
+#include <stdio.h>
+
+using std::string;
+using std::vector;
+
+std::vector<string> vec;
+
+void findfile(string path, string mode) {
+	_finddata_t file;
+	intptr_t HANDLE;
+	string Onepath = path + mode;
+	HANDLE = _findfirst(Onepath.c_str(), &file);
+	if (HANDLE == -1L)
+	{
+		cout << "can not match the folder path" << endl;
+		system("pause");
+	}
+	do {
+		//判断是否有子目录  
+		if (file.attrib & _A_SUBDIR)
+		{
+			//判断是否为"."当前目录，".."上一层目录
+			if ((strcmp(file.name, ".") != 0) && (strcmp(file.name, "..") != 0))
+			{
+				string newPath = path + "\\" + file.name;
+				findfile(newPath, mode);
+			}
+		}
+		else
+		{
+			cout << file.name << " " << endl;
+			/*if (string::npos != string(file.name).substr())*/ {
+				vec.push_back(string(file.name));
+			}
+		}
+	} while (_findnext(HANDLE, &file) == 0);
+	_findclose(HANDLE);
 }
 
-int main() {
-	char* c = '\0';
-	char s = 'k';
-	set(c, s);
-	cout << c;
+int main(int argc, char** argv) {
+	string mode = "\\*.*";
+	string path = "E:\\Movie";
+	findfile(path, mode);
+	cout << endl;
+	for (auto item : vec) {
+		cout << item << endl;
+	}
+	system("pause");
 	return 0;
 }
